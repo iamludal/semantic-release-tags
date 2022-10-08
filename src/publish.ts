192dcb9a32639fc6e8998a1b94184bf3424e8e71
Ludal <44783088+iamludal@users.noreply.github.com>
2022-10-08 23:02:08 +0200

    fix: also move minor tags (#2)
    BREAKING CHANGE: minor tags are now added

--- src/publish.ts
+++ src/publish.ts
@@ -1,10 +1,23 @@
 import { Config, Context } from 'semantic-release';
 import { execSync } from 'child_process';
 
-export async function publish(_: Config, { nextRelease, options }: Context) {
-  const major = nextRelease.version.split('.')[0];
+export async function publish(_: Config, { nextRelease, options, logger }: Context) {
+  const { version } = nextRelease;
+
+  // In case of pre-release, don't move tags
+  if (version.includes('-')) {
+    logger.log('Pre-release detected, skipping tag move');
+    return;
+  }
+
+  const [majorNumber, minorNumber] = version.split('.').slice(0, 2);
   const commit = nextRelease.gitHead;
-  const finalTag = options.tagFormat.replace('${version}', major);
-  execSync(`git tag --force --message ${finalTag} ${finalTag} ${commit}`);
-  execSync(`git push origin ${finalTag} --force`);
+  const majorTag = options.tagFormat.replace('${version}', majorNumber);
+  const minorTag = options.tagFormat.replace('${version}', `${majorNumber}.${minorNumber}`);
+
+  logger.log(`Tagging commit ${commit} with ${majorTag} and ${minorTag}`);
+
+  execSync(`git tag --force --message ${majorTag} ${majorTag} ${commit}`);
+  execSync(`git tag --force --message ${minorTag} ${minorTag} ${commit}`);
+  execSync(`git push origin ${majorTag} ${minorTag} --force`);
 }
