9a993cbb402f9c1a21a9fdeac2dfd4bcb5a9d669
Ludal <ludovic.chombeau@gmail.com>
2022-10-08 22:38:25 +0200

    fix: don't move tags in case of pre-release

--- src/publish.ts
+++ src/publish.ts
@@ -1,11 +1,22 @@
 import { Config, Context } from 'semantic-release';
 import { execSync } from 'child_process';
 
-export async function publish(_: Config, { nextRelease, options }: Context) {
-  const [majorNumber, minorNumber] = nextRelease.version.split('.').slice(0, 2);
+export async function publish(_: Config, { nextRelease, options, logger }: Context) {
+  const { version } = nextRelease;
+
+  // In case of pre-release, don't move tags
+  if (version.includes('-')) {
+    logger.log('Pre-release detected, skipping tag move');
+    return;
+  }
+
+  const [majorNumber, minorNumber] = version.split('.').slice(0, 2);
   const commit = nextRelease.gitHead;
   const majorTag = options.tagFormat.replace('${version}', majorNumber);
   const minorTag = options.tagFormat.replace('${version}', `${majorNumber}.${minorNumber}`);
+
+  logger.log(`Tagging commit ${commit} with ${majorTag} and ${minorTag}`);
+
   execSync(`git tag --force --message ${majorTag} ${majorTag} ${commit}`);
   execSync(`git tag --force --message ${minorTag} ${minorTag} ${commit}`);
   execSync(`git push origin ${majorTag} ${minorTag} --force`);
