79c05863852294ed6571f4e87baea34f20e9fb48
Ludal <ludovic.chombeau@gmail.com>
2022-10-08 11:28:23 +0200

    fix: also move minor tag
    BREAKING CHANGE: a new tag is created, corresponding to
    the minor version

--- src/publish.ts
+++ src/publish.ts
@@ -2,9 +2,11 @@ import { Config, Context } from 'semantic-release';
 import { execSync } from 'child_process';
 
 export async function publish(_: Config, { nextRelease, options }: Context) {
-  const major = nextRelease.version.split('.')[0];
+  const [majorNumber, minorNumber] = nextRelease.version.split('.').slice(0, 2);
   const commit = nextRelease.gitHead;
-  const finalTag = options.tagFormat.replace('${version}', major);
-  execSync(`git tag --force --message ${finalTag} ${finalTag} ${commit}`);
-  execSync(`git push origin ${finalTag} --force`);
+  const majorTag = options.tagFormat.replace('${version}', majorNumber);
+  const minorTag = options.tagFormat.replace('${version}', `${majorNumber}.${minorNumber}`);
+  execSync(`git tag --force --message ${majorTag} ${majorTag} ${commit}`);
+  execSync(`git tag --force --message ${minorTag} ${minorTag} ${commit}`);
+  execSync(`git push origin ${majorTag} ${minorTag} --force`);
 }
